d4e63d65cbce3737d52d6347b81eb932eb49c0ba
Frank van Meurs <fvm@frankvanmeurs.nl>
2019-03-27 13:56:22 +0100

    Cyclic rotation

---
+++ internal/pkg/cyclic-rotation/solution/solution.go
@@ -0,0 +1,18 @@
+package solution
+
+import "container/ring"
+
+func Solution(A []int, K int) []int {
+	// write your code in Go 1.4
+	r := ring.New(len(A))
+	for i := range A {
+		r.Value = A[i]
+		r = r.Next()
+	}
+	r = r.Move(K)
+	for i := 0; i < r.Len(); i++ {
+		A[i] = r.Value.(int)
+		r = r.Next()
+	}
+	return A
+}
