173a8b4a380bcd2ae05f0c1b1994da71fc5fe644
Frank van Meurs <fvm@frankvanmeurs.nl>
2019-03-27 14:25:29 +0100

    Layout and comments

--- internal/pkg/cyclic-rotation/solution/solution.go
+++ internal/pkg/cyclic-rotation/solution/solution.go
@@ -3,19 +3,26 @@ package solution
 import "container/ring"
 
 func Solution(A []int, K int) []int {
+	// Boooooo!
 	if len(A) == 0 || K == 0 {
 		return A
 	}
 
 	r := ring.New(len(A))
+	// Add all the ints
 	for i := range A {
 		r.Value = A[i]
 		r = r.Next()
 	}
-	r = r.Move(-1 * K) // Move **RIGHT** (not left)
+
+	// Move **RIGHT** (not left)
+	r = r.Move(-1 * K)
+
+	// Remove all the ints
 	for i := 0; i < r.Len(); i++ {
 		A[i] = r.Value.(int)
 		r = r.Next()
 	}
+
 	return A
 }
