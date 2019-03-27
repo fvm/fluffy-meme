5f43aa6fd78912634065eb3067f16eace27e58ac
Frank van Meurs <fvm@frankvanmeurs.nl>
2019-03-27 14:26:09 +0100

    Layout and comments

--- internal/pkg/cyclic-rotation/solution/solution.go
+++ internal/pkg/cyclic-rotation/solution/solution.go
@@ -3,12 +3,14 @@ package solution
 import "container/ring"
 
 func Solution(A []int, K int) []int {
-	// Boooooo!
+	// Booooooh!
 	if len(A) == 0 || K == 0 {
 		return A
 	}
 
+	// Get a ring
 	r := ring.New(len(A))
+
 	// Add all the ints
 	for i := range A {
 		r.Value = A[i]
